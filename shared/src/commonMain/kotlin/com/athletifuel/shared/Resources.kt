b55990ea185067a980eb6539565460dbee53192c
Abdsayed <Abdelhameed.sayed.ext@orange.com>
2026-05-09 10:11:32 +0300

    Add core design system resources and shared module configuration to the AthletiFuel KMP project

---
+++ shared/src/commonMain/kotlin/com/athletifuel/shared/Resources.kt
@@ -0,0 +1,64 @@
+package com.athletifuel.shared
+
+import athletifuel.shared.generated.resources.Res
+import athletifuel.shared.generated.resources.back_arrow
+import athletifuel.shared.generated.resources.book
+import athletifuel.shared.generated.resources.cat
+import athletifuel.shared.generated.resources.check
+import athletifuel.shared.generated.resources.checkmark_image
+import athletifuel.shared.generated.resources.close
+import athletifuel.shared.generated.resources.delete
+import athletifuel.shared.generated.resources.dollar
+import athletifuel.shared.generated.resources.edit
+import athletifuel.shared.generated.resources.google_logo
+import athletifuel.shared.generated.resources.grid
+import athletifuel.shared.generated.resources.home
+import athletifuel.shared.generated.resources.log_in
+import athletifuel.shared.generated.resources.log_out
+import athletifuel.shared.generated.resources.map_pin
+import athletifuel.shared.generated.resources.menu
+import athletifuel.shared.generated.resources.minus
+import athletifuel.shared.generated.resources.paypal_logo
+import athletifuel.shared.generated.resources.plus
+import athletifuel.shared.generated.resources.right_arrow
+import athletifuel.shared.generated.resources.search
+import athletifuel.shared.generated.resources.shopping_cart
+import athletifuel.shared.generated.resources.shopping_cart_image
+import athletifuel.shared.generated.resources.unlock
+import athletifuel.shared.generated.resources.user
+import athletifuel.shared.generated.resources.vertical_menu
+import athletifuel.shared.generated.resources.warning
+
+object Resources {
+    object Icon {
+        val Plus = Res.drawable.plus
+        val Minus = Res.drawable.minus
+        val SignIn = Res.drawable.log_in
+        val SignOut = Res.drawable.log_out
+        val Unlock = Res.drawable.unlock
+        val Search = Res.drawable.search
+        val Person = Res.drawable.user
+        val Checkmark = Res.drawable.check
+        val Edit = Res.drawable.edit
+        val Menu = Res.drawable.menu
+        val BackArrow = Res.drawable.back_arrow
+        val RightArrow = Res.drawable.right_arrow
+        val Home = Res.drawable.home
+        val ShoppingCart = Res.drawable.shopping_cart
+        val Categories = Res.drawable.grid
+        val Dollar = Res.drawable.dollar
+        val MapPin = Res.drawable.map_pin
+        val Close = Res.drawable.close
+        val Book = Res.drawable.book
+        val VerticalMenu = Res.drawable.vertical_menu
+        val Delete = Res.drawable.delete
+        val Warning = Res.drawable.warning
+    }
+    object Image {
+        val ShoppingCart = Res.drawable.shopping_cart_image
+        val Checkmark = Res.drawable.checkmark_image
+        val Cat = Res.drawable.cat
+        val GoogleLogo = Res.drawable.google_logo
+        val PaypalLogo = Res.drawable.paypal_logo
+    }
+}
